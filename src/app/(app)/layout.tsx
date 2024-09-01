a8896d18f89daa7d89490b927e3f767ee6fa9e00
UmairRaaz <umairgopang123@gmail.com>
2024-09-01 18:15:54 +0500

    remove color

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
             visible={true}
             height="80"
             width="80"
-            color="#4fa94d"
             ariaLabel="progress-bar-loading"
             wrapperStyle={{}}
             wrapperClass=""
